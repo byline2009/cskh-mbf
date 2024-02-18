42fc3693655ff176c104e769375ebc7a3da8acbf
Admin <admin@MacBook-Pro-cua-Admin.local>
2024-02-18 23:45:18 +0700

    update ;ayout

--- src/app/cau-hoi-thuong-gap/page.tsx
+++ src/app/cau-hoi-thuong-gap/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { questions1 } from "@config/constants";
 import React, { useEffect } from "react";
 
 const Page = () => {
@@ -17,6 +18,23 @@ const Page = () => {
         contents[index].classList.add("active");
       });
     });
+    tabs[0].classList.add("active");
+    contents[0].classList.add("active");
+    var accordions = document.querySelectorAll("accordion");
+    accordions.forEach((acc, index) => {
+      accordions.forEach((acc1) => {
+        acc1.classList.remove("active");
+      });
+      acc.addEventListener("click", (e) => {
+        acc.classList.add("active");
+      });
+    });
+
+    return () => {
+      tabs.forEach((tab, index) => {
+        tab.removeEventListener("click", () => {});
+      });
+    };
   }, []);
   return (
     <div className="questions">
@@ -31,21 +49,127 @@ const Page = () => {
         <div className="content_box">
           <div className="content">
             <h2>Gói cước</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
-              molestiae ipsa cupiditate reiciendis aliquam expedita unde esse,
-              culpa quidem blanditiis, dolor assumenda deserunt reprehenderit
-              eos sunt, ipsum maiores dolorem nihil.
-            </p>
+            {/* {questions1.map((item, index) => (
+              <div className="faq" key={index}>
+                <button className="accordion">
+                  {item.q} <i className="fa-solid fa-chevron-down"></i>
+                </button>
+                <div className="pannel">
+                  <p>{item.a}</p>
+                </div>
+              </div>
+            ))} */}
+            <div id="accordion">
+              <div className="card">
+                <div className="card-header" id="headingOne">
+                  <h5 className="mb-0">
+                    <button
+                      className="btn btn-link"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Collapsible Group Item #1
+                    </button>
+                  </h5>
+                </div>
+
+                <div
+                  id="collapseOne"
+                  className="collapse show"
+                  aria-labelledby="headingOne"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    {`Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non cupidatat skateboard dolor brunch. Food truck
+                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                    sunt aliqua put a bird on it squid single-origin coffee
+                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                    Leggings occaecat craft beer farm-to-table, raw denim
+                    aesthetic synth nesciunt you probably haven't heard of them
+                    accusamus labore sustainable VHS.`}
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header" id="headingTwo">
+                  <h5 className="mb-0">
+                    <button
+                      className="btn btn-link collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      Collapsible Group Item #2
+                    </button>
+                  </h5>
+                </div>
+                <div
+                  id="collapseTwo"
+                  className="collapse"
+                  aria-labelledby="headingTwo"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    {`Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non cupidatat skateboard dolor brunch. Food truck
+                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                    sunt aliqua put a bird on it squid single-origin coffee
+                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                    Leggings occaecat craft beer farm-to-table, raw denim
+                    aesthetic synth nesciunt you probably haven't heard of them
+                    accusamus labore sustainable VHS.`}
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header" id="headingThree">
+                  <h5 className="mb-0">
+                    <button
+                      className="btn btn-link collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      Collapsible Group Item #3
+                    </button>
+                  </h5>
+                </div>
+                <div
+                  id="collapseThree"
+                  className="collapse"
+                  aria-labelledby="headingThree"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    {` Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non cupidatat skateboard dolor brunch. Food truck
+                    quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                    sunt aliqua put a bird on it squid single-origin coffee
+                    nulla assumenda shoreditch et. Nihil anim keffiyeh
+                    helvetica, craft beer labore wes anderson cred nesciunt
+                    sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                    Leggings occaecat craft beer farm-to-table, raw denim
+                    aesthetic synth nesciunt you probably haven't heard of them
+                    accusamus labore sustainable VHS.`}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="content">
             <h2>Dịch vụ</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
-              molestiae ipsa cupiditate reiciendis aliquam expedita unde esse,
-              culpa quidem blanditiis, dolor assumenda deserunt reprehenderit
-              eos sunt, ipsum maiores dolorem nihil.
-            </p>
           </div>
           <div className="content">
             <h2>Thanh toán cước</h2>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 "use client";
 
 import "../styles/global.scss";
@@ -24,8 +25,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" type="image/png" href="imgs/favicon.png" />
-
+      <title>Chăm sóc khách hàng Công ty MobiFone 7</title>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />{" "}
+      </head>
       <body>
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossOrigin="anonymous"
+        ></script>
         <HeaderApp toggleMenu={toggleDrawer} isOpen={isOpen} />
         <div className="main-layout">
           <div className="content-page">{children}</div>
