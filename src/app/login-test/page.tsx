ab3ba6f5cd1ef795264a9014144cc5e7091242d5
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-03-05 16:44:43 +0700

    update layout

--- src/app/login-test/page.tsx
+++ src/app/login-test/page.tsx
@@ -1,6 +1,28 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [typePassword, setTypePassword] = useState("password");
+  useEffect(() => {}, []);
+  const handleNextClick = (e: any) => {
+    e.preventDefault();
+    const pageBox = document.querySelector(".page-box");
+    pageBox?.classList.add("active-pass");
+  };
+  const handleBackClick = (e: any) => {
+    e.preventDefault();
+    const pageBox = document.querySelector(".page-box");
+    pageBox?.classList.remove("active-pass");
+  };
+  const handleShowPass = (e: any) => {
+    let isChecked = e.target.checked;
+    if (isChecked) {
+      setTypePassword("text");
+    } else {
+      setTypePassword("password");
+    }
+    console.log(e);
+  };
   return (
     <div className="login-test">
       <div className="container-login">
@@ -11,9 +33,9 @@ const page = () => {
                 <h2>Login</h2>
                 <p>Please login to use platform</p>
               </div>
-              <div className="page">
+              <div className="page email-page">
                 <div className="input-box">
-                  <input type="text" className="email" required />
+                  <input type="text" className="email" autoFocus required />
                   <label htmlFor="email">Enter your email</label>
                 </div>
                 <div className="forgot">
@@ -25,7 +47,38 @@ const page = () => {
                 </div>
                 <div className="btn-box">
                   <a href="#">Create account</a>
-                  <button className="bnt-next">Next</button>
+                  <button onClick={handleNextClick} className="bnt-next">
+                    Next
+                  </button>
+                </div>
+              </div>
+              <div className="page password-page">
+                <div className="input-box">
+                  <input type={typePassword} className="password" required />
+                  <label htmlFor="password">Enter your password</label>
+                </div>
+                <div className="forgot show">
+                  <a href="#">Forgot password?</a>
+                  <label>
+                    <input
+                      type="checkbox"
+                      className="checkbox-pass"
+                      onClick={handleShowPass}
+                    />
+                    Show password
+                  </label>
+                </div>
+                <div className="guest-mode">
+                  <p>Not your computer? Use guest mode to login privately</p>
+                  <a href="#">Learn more</a>
+                </div>
+                <div className="btn-box">
+                  <button onClick={handleBackClick} className="bnt-back">
+                    Back
+                  </button>
+                  <button className="bnt-next" type="submit">
+                    Login
+                  </button>
                 </div>
               </div>
             </div>
@@ -36,4 +89,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
