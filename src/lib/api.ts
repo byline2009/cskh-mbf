3577cd776803319bed16640907c403de490e9bd7
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-05-29 15:02:52 +0700

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -20,7 +20,7 @@ axios.interceptors.response.use(
 );
 // Server should return AuthModel
 export function login(email: string, password: string) {
-  return axios.post("http://localhost:8100/login", {
+  return postData("http://localhost:8100/login", {
     username: email,
     password,
   });
@@ -72,4 +72,22 @@ export function getUserByToken(token: string) {
 
 export function checkPackage(searchVal?: string) {
   return axios.get(CHECK_PACKAGE_URL + `?isdn=${searchVal}`);
+}
+
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
