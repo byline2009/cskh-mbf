ed08ec0e5bf19623a28f513456ff4bff3e82becb
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-10-23 11:52:05 +0700

    update

--- src/scripts/pinecone-prepare-docs.ts
+++ src/scripts/pinecone-prepare-docs.ts
@@ -87,7 +87,7 @@ import {
     };
 
     const config: PineconeConfiguration = {
-      apiKey: env.OPENAI_API_KEY,
+      apiKey: "409e625d-dec0-4241-88bc-30efca393b76",
       fetchApi: customFetch,
     };
 
