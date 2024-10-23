b2de20e04060c42e3f9d5835c9d61679773e8e0c
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-10-23 15:56:43 +0700

    u

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -5,16 +5,16 @@ import { getPineconeClient } from "../../lib/pinecone-client";
 
 const Page = () => {
   const [check, setCheck] = useState();
-  useEffect(() => {
-    async function fetchData() {
-      // You can await here
-      const pc = await getPineconeClient();
-      const listIndex = await pc.listIndexes();
-      setCheck(listIndex[0].name);
-      // ...
-    }
-    fetchData();
-  }, []);
+  
+   async function fetchData() {
+     // You can await here
+     const pc = await getPineconeClient();
+     const listIndex = await pc.listIndexes();
+     setCheck(listIndex[0].name);
+
+     // ...
+   }
+   fetchData();
   return (
     <div className="container">
       <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
