7b2815764cd668d9d17614b788aa703129a938a8
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-10-27 16:42:39 +0700

    u

--- src/app/api/checkPackage/route.js
+++ src/app/api/checkPackage/route.js
@@ -17,9 +17,9 @@ export async function GET(req, context) {
     );
   }
 }
-export function getQSParamFromURL(key, url) {
-  if (!url) return "";
-  const search = new URL(url).search;
-  const urlParams = new URLSearchParams(search);
-  return urlParams.get(key);
-}
+ function getQSParamFromURL(key, url) {
+   if (!url) return "";
+   const search = new URL(url).search;
+   const urlParams = new URLSearchParams(search);
+   return urlParams.get(key);
+ }
