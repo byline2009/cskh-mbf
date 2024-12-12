a76656c63c7bfad3557c2f1ab0ace62408bd4665
Huy Pham <phamgiahuy9h@gmail.com>
2024-12-12 15:18:42 +0700

    change cfnext

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // experimental: {
+  //   serverActions: true,
+  // },
   reactStrictMode: false,
   env: {
     NEXTAUTH_APP_API_URL: process.env.NEXTAUTH_APP_API_URL,
