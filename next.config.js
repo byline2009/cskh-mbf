bbd693a601bdf316f4da9095ba8d2b13eca2eb47
Huy Pham <phamgiahuy9h@gmail.com>
2024-12-12 18:37:40 +0700

    change nextcf1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   serverActions: true,
-  // },
+  experimental: {
+    serverActions: true,
+  },
   reactStrictMode: false,
   env: {
     NEXTAUTH_APP_API_URL: process.env.NEXTAUTH_APP_API_URL,
@@ -10,19 +10,6 @@ const nextConfig = {
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
-  },
-  async rewrites() {
-    const apiBaseUrl = process.env.NEXTAUTH_APP_API_URL; // Lấy từ biến môi trường
-    if (!apiBaseUrl) {
-      throw new Error("NEXTAUTH_APP_API_URL không được thiết lập trong .env!");
-    }
-
-    return [
-      {
-        source: '/api/:path*', // Proxy mọi request bắt đầu với /api
-        destination: `${apiBaseUrl}/:path*`, // Chuyển hướng tới backend API
-      },
-    ];
   },
 };
 
