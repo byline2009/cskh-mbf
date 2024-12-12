37e16c79f4e890707cd2af7a287184b10ed41d6c
Huy Pham <phamgiahuy9h@gmail.com>
2024-12-12 18:33:00 +0700

    change nextconfig

--- next.config.js
+++ next.config.js
@@ -11,6 +11,19 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
+  async rewrites() {
+    const apiBaseUrl = process.env.NEXTAUTH_APP_API_URL; // Lấy từ biến môi trường
+    if (!apiBaseUrl) {
+      throw new Error("NEXTAUTH_APP_API_URL không được thiết lập trong .env!");
+    }
+
+    return [
+      {
+        source: '/api/:path*', // Proxy mọi request bắt đầu với /api
+        destination: `${apiBaseUrl}/:path*`, // Chuyển hướng tới backend API
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
